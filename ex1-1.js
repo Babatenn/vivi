22c1c7757e4d7ff8d053fe2907709c8dbf5039fc
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-14 21:55:12 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,2 +1,2 @@
 function imgchange1(){document.getElementById('mark').src='./image/セブチ1.jpg';}
-function imgchange2(){document.getElementById('mark').src='./image/セブチ2.jpg';}
+function imgchange2(){document.getElementById('mark').src='./image/セブチ3.jpg';}
